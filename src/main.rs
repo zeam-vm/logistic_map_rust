e25af5ca5c3c6a040583c38851a20eee2ad37620
Susumu Yamazaki <zacky1972@gmail.com>
2018-05-31 09:00:18 +0900

    support ocl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate time;
+extern crate ocl;
+
+use ocl::{ProQue, Buffer, MemFlags};
 
 const LOOP: usize = 10;
 const P: i64 = 6_700_417;
@@ -14,12 +17,69 @@ fn logisticsmap_loop_calc(num: usize, x: i64, p: i64, mu: i64) -> i64 {
 }
 
 fn benchmark() {
-    let mut x = (0..NUM_VALUES as i64).collect::<Vec<_>>();
-    for xi in &mut x[1..] {
-        *xi = logisticsmap_loop_calc(LOOP, *xi, P, MU)
+    let r1 = logistic_map_ocl((0..NUM_VALUES as i64).collect::<Vec<_>>(), P, MU);
+
+    match r1 {
+        Ok(r2) => {
+            println!("1: {}, 10000: {}", r2[1], r2[10000]);
+        },
+       Err(_) => {
+            println!("error!");
+       },
     }
-    println!("1: {}, 10000: {}", x[1], x[10000]);
 }
+
+fn logistic_map_ocl(x: Vec<i64>, p: i64, mu: i64) -> ocl::Result<(Vec<i64>)> {
+    let src = r#"
+        __kernel void calc(__global long* input, __global long* output, long p, long mu) {
+            size_t i = get_global_id(0);
+            long x = input[i];
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            x = mu * x * (x + 1) % p;
+            output[i] = x;
+        }
+    "#;
+
+    let pro_que = ProQue::builder()
+        .src(src)
+        .dims(x.len())
+        .build().expect("Build ProQue");
+
+    let source_buffer = Buffer::builder()
+        .queue(pro_que.queue().clone())
+        .flags(MemFlags::new().read_write())
+        .len(x.len())
+        .copy_host_slice(&x)
+        .build()?;
+
+    let result_buffer: Buffer<i64> = Buffer::builder()
+        .queue(pro_que.queue().clone())
+        .flags(MemFlags::new().read_write())
+        .len(x.len())
+        .build()?;
+
+    let kernel = pro_que.kernel_builder("calc")
+        .arg(&source_buffer)
+        .arg(&result_buffer)
+        .arg(p)
+        .arg(mu)
+        .build()?;
+
+    unsafe { kernel.enq()?; }
+
+    let mut vec_result = vec![0; result_buffer.len()];
+    result_buffer.read(&mut vec_result).enq()?;
+    Ok(vec_result)
+}
+
 
 fn main() {
     let start_time = time::get_time();
