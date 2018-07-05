9fe18f4f4f7481c5238febd03c925e2c88b7bbe3
Susumu Yamazaki <zacky1972@gmail.com>
2018-07-05 09:59:28 +0900

    add rayon for parallel execution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate time;
 extern crate ocl;
+extern crate rayon;
 
 use ocl::{ProQue, Buffer, MemFlags};
+use rayon::prelude::*;
 
 const LOOP: usize = 10;
 const P: i64 = 6_700_417;
@@ -16,7 +18,12 @@ fn logisticsmap_loop_calc(num: usize, x: i64, p: i64, mu: i64) -> i64 {
     (0..num).fold(x, |x, _| logisticsmap_calc(x, p, mu))
 }
 
-fn benchmark() {
+fn benchmark_cpu() {
+    let r1 = (0..NUM_VALUES as i64).collect::<Vec<_>>().par_iter().map(|&x| logisticsmap_loop_calc(LOOP, x, P, MU)).collect::<Vec<i64>>();
+    println!("1: {}, 10000: {}", r1[1], r1[10000]);
+}
+
+fn benchmark_gpu() {
     let r1 = logistic_map_ocl((0..NUM_VALUES as i64).collect::<Vec<_>>(), P, MU);
 
     match r1 {
@@ -28,6 +35,7 @@ fn benchmark() {
        },
     }
 }
+
 
 fn logistic_map_ocl(x: Vec<i64>, p: i64, mu: i64) -> ocl::Result<(Vec<i64>)> {
     let src = r#"
@@ -82,11 +90,23 @@ fn logistic_map_ocl(x: Vec<i64>, p: i64, mu: i64) -> ocl::Result<(Vec<i64>)> {
 
 
 fn main() {
-    let start_time = time::get_time();
-    benchmark();
-    let end_time = time::get_time();
-    let diffsec = end_time.sec - start_time.sec;   // i64
-    let diffsub = end_time.nsec - start_time.nsec; // i32
-    let realsec = diffsec as f64 + diffsub as f64 * 1e-9;
-    println!("{:.6} sec", realsec);
+    {
+        let start_time = time::get_time();
+        benchmark_gpu();
+        let end_time = time::get_time();
+        let diffsec = end_time.sec - start_time.sec;   // i64
+        let diffsub = end_time.nsec - start_time.nsec; // i32
+        let realsec = diffsec as f64 + diffsub as f64 * 1e-9;
+        println!("GPU: {:.6} sec", realsec);
+    }
+    {
+        let _ = rayon::ThreadPoolBuilder::new().num_threads(32).build_global().unwrap();
+        let start_time = time::get_time();
+        benchmark_cpu();
+        let end_time = time::get_time();
+        let diffsec = end_time.sec - start_time.sec;   // i64
+        let diffsub = end_time.nsec - start_time.nsec; // i32
+        let realsec = diffsec as f64 + diffsub as f64 * 1e-9;
+        println!("CPU: {:.6} sec", realsec);        
+    }
 }
