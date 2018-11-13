b8f46fe4975ded90e5168bda93fe4c6e085ba383
zacky1972 <zacky1972@gmail.com>
2018-11-14 01:10:21 +0900

    create gpu_info

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate num_cpus;
 extern crate ocl;
 
 use ocl::{ProQue, Buffer, MemFlags};
+use ocl::{Platform, Device};
+use ocl::enums::{PlatformInfo, DeviceInfo, DeviceInfoResult};
 use rayon::prelude::*;
 use rayon::ThreadPool;
 use rayon::ThreadPoolBuildError;
@@ -47,15 +49,66 @@ fn benchmark(n: usize) -> Result<f64, ThreadPoolBuildError> {
     }
 }
 
-fn benchmark_gpu() {
+fn work_dim(device: &Device) -> u32 {
+	match device.info(DeviceInfo::MaxWorkItemDimensions) {
+		Ok(DeviceInfoResult::MaxWorkItemDimensions(res)) => res,
+    _ => {
+      println!("failed to get DeviceInfoResult::MaxWorkItemDimensions");
+      1
+    },
+  }
+}
+
+fn max_work_group_size(device: &Device) -> usize {
+	match device.info(DeviceInfo::MaxWorkGroupSize) {
+		Ok(DeviceInfoResult::MaxWorkGroupSize(res)) => res,
+		_ => {
+			println!("failed to get DeviceInfoResult::MaxWorkGroupSize");
+			1
+    },
+  }
+}
+
+fn compute_unit_num(device: &Device) -> u32 {
+	match device.info(DeviceInfo::MaxComputeUnits) {
+		Ok(DeviceInfoResult::MaxComputeUnits(res)) => res,
+    _ => {
+      println!("failed to get DeviceInfoResult::MaxComputeUnits");
+      0
+    },
+  }
+}
+
+fn max_local_memory_size(device: &Device) -> u32 {
+	match device.info(DeviceInfo::LocalMemSize) {
+		Ok(DeviceInfoResult::LocalMemSize(res)) => res as u32,
+    _ => {
+      println!("failed to get DeviceInfoResult::LocalMemSize");
+      0
+    },
+  }
+}
+
+fn max_work_item_size(device: &Device) -> Vec<usize> {
+	match device.info(DeviceInfo::MaxWorkItemSizes) {
+		Ok(DeviceInfoResult::MaxWorkItemSizes(res)) => res,
+    _ => {
+      println!("failed to get DeviceInfoResult::MaxWorkGroupSize");
+      vec![1; 3]
+    },
+  }
+}
+
+fn benchmark_gpu() -> i64 {
     let r1 = logistic_map_ocl((0..NUM_VALUES as i64).collect::<Vec<_>>(), P, MU);
 
     match r1 {
         Ok(r2) => {
-            println!("1: {}, 10000: {}", r2[1], r2[10000]);
+            r2[10000]
         },
        Err(_) => {
             println!("error!");
+            0
        },
     }
 }
@@ -112,8 +165,51 @@ fn logistic_map_ocl(x: Vec<i64>, p: i64, mu: i64) -> ocl::Result<(Vec<i64>)> {
     Ok(vec_result)
 }
 
+pub fn gpuinfo() {
+  let platforms = Platform::list();
+
+  println!("platform num:{:?}", platforms.len());
+
+  for p_idx in 0..platforms.len() {
+  	let platform = &platforms[p_idx];
+  	let devices = Device::list_all(platform).unwrap();
+  	if devices.is_empty() { continue; }
+  	println!("Profile:{:?}", platform.info(PlatformInfo::Profile));
+  	println!("Version:{:?}", platform.info(PlatformInfo::Version));
+  	println!("Name:{:?}", platform.info(PlatformInfo::Name));
+  	println!("Vendor:{:?}", platform.info(PlatformInfo::Vendor));
+  	for device in devices.iter() {
+  		println!("----");
+  		println!("Device Name:{:?}", device.name().unwrap());
+  		println!("Device Vendor:{:?}", device.vendor().unwrap());
+  		println!("  work_dim: {:?}", work_dim(device));
+  		println!("  compute_unit_num: {:?}", compute_unit_num(device));
+  		println!("  max_work_group_size: {:?}", max_work_group_size(device));
+  		println!("  max_local_memory_size: {:?}", max_local_memory_size(device));
+  		// max_local_memory_size を超えたデータを
+  		// ローカルメモリ(__local修飾子)としてカーネルに渡すと
+  		// エラーが発生する．
+  		println!("  max_work_item_size: {:?}", max_work_item_size(device));
+  	}
+  }
+}
 
 fn main() {
+	{
+		gpuinfo();
+  	let minsec = (1..=10).collect::<Vec<_>>().iter().map(|_|
+  	{
+    	let start_time = time::get_time();
+    	benchmark_gpu();
+    	let end_time = time::get_time();
+    	let diffsec = end_time.sec - start_time.sec;   // i64
+    	let diffsub = end_time.nsec - start_time.nsec; // i32
+    	let realsec = diffsec as f64 + diffsub as f64 * 1e-9;
+    	realsec
+  	}).fold(0.0/0.0, |m, v| v.min(m));
+  	println!("GPU: {}", (minsec * 1000.0).round() / 1000.0);
+	}
+
 	let num = num_cpus::get();
 	let mut single:f64 = 0.0;
 
@@ -131,5 +227,6 @@ fn main() {
 			_ => {},
 		}
 		println!(", {}, {}", n, ((single / minsec / (n as f64)) * 1000.0).round() / 10.0 );
-	})
+	});
+
 }
